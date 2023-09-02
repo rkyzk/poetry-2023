2d45f466177b08707e890ae0667eb3c73e0463c3
rkyzk <reikoyazaki@yahoo.com>
2023-09-02 02:16:43 +0000

    make navbar and style it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import styles from "./App.css";
+import styles from "./App.module.css";
 import { Route, Switch } from "react-router-dom";
 import Home from "./pages/poems/Home";
+import NavBar from "./components/NavBar";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
     <div className={styles.App}>
-      <Switch>
-        <Route exact path="/" render={() => <Home />} />
-      </Switch>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <Home />} />
+        </Switch>
+      </Container>
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
+import logo from "../assets/media/poems-logo.png";
+
+/**
+ *  Render the first navbar on top right.
+ */
+const NavBar = () => {
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <NavLink exact to="/">
+          <Navbar.Brand className={styles.Logo}>
+            <img src={logo} alt="logo" />
+          </Navbar.Brand>
+        </NavLink>
+        <h1 className="mt-4">Your Poetry</h1>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" id="navbar-toggle">
+          <i className={`${styles.Burger} fa-solid fa-bars`}></i>
+        </Navbar.Toggle>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <NavLink exact className={`${styles.NavLink} mr-3`} to="/">
+              Home
+            </NavLink>
+            <NavLink exact className={`${styles.NavLink} mr-3`} to="#">
+              Contact
+            </NavLink>
+            <NavLink exact className={`${styles.NavLink} mr-3`} to="#">
+              Sign in
+            </NavLink>
+            <NavLink exact className={`${styles.NavLink} mr-3`} to="#">
+              Sign up
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar;
