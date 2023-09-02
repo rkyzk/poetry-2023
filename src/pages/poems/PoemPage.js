768e1ce87aef97cdd88de8f9182630eea03f8b33
rkyzk <reikoyazaki@yahoo.com>
2023-09-02 12:56:22 +0000

    add comment form on PoemPage

--- src/pages/poems/PoemPage.js
+++ src/pages/poems/PoemPage.js
@@ -4,6 +4,8 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { axiosReq } from "../../api/axiosDefaults";
 import Poem from "./Poem";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import CommentCreateForm from "../comments/CommentCreateForm";
 
 /**
  * Return content of individual poem pages.
@@ -13,6 +15,12 @@ function PoemPage() {
   const { id } = useParams();
   /** stores data of the poem */
   const [poem, setPoem] = useState({ results: [] });
+  /** stores info about the logged in user. */
+  const currentUser = useCurrentUser();
+  /** stores profile image of the current user */
+  const profile_image = currentUser?.profile_image;
+  /** stores comments about the poem */
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     /** get the data of the poem and the comments and set them to variables */
@@ -36,6 +44,17 @@ function PoemPage() {
       <Row className="h-100">
         <Col className="mt-3" lg={{ span: 8, offset: 2 }}>
           <Poem {...poem.results[0]} setPoems={setPoem} poemPage />
+          {/* If logged in, display comment form.
+                If not, display the heading 'Comments' if there are any comments. */}
+          {currentUser && (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              poem={id}
+              setPoem={setPoem}
+              setComments={setComments}
+            />
+          )}
         </Col>
       </Row>
     </>
