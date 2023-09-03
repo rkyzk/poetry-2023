fb3874f234cb993bcae9d9d5a04d7b866b83bda7
rkyzk <reikoyazaki@yahoo.com>
2023-09-03 01:51:02 +0000

    add intro and featured poems section on Home

--- src/pages/poems/Home.js
+++ src/pages/poems/Home.js
@@ -3,20 +3,44 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import FeaturedProfiles from "../profiles/FeaturedProfiles";
+import styles from "../../styles/Home.module.css";
+import PoemsPage from "./PoemsPage";
 
 /**
  * Return the content of "Home" page.
  */
 function Home() {
+  /** The heading and the introduction. */
+  const intro = (
+    <>
+      <h2 className="text-center">Welcome!</h2>
+      <p className={styles.Intro}>
+        You landed this page, because you love poetry. In this friednly and
+        supportive community you can share your poems, read others' poems, give
+        and receive feedback. Haven't written since high school years? Or
+        haven't written a poem before? Doesn't matter. Read some poems, get
+        inspired and start writing!
+      </p>
+    </>
+  );
+
   return (
     <Container>
       <Row>
-        <Col lg={8}>
-          <p>Featured Profiles for mobile</p>
-          <p>Intro</p>
-          <p>Featured Poems</p>
+        <Col className="py-2 p-0 p-lg-2" lg={8}>
+          {/* display featured profiles at the top center for
+            screen sizes md or smaller. */}
+          <FeaturedProfiles mobile />
+          {intro}
+          <hr className="my-4" />
+          {/* featured poems section */}
+          <PoemsPage
+            filter={`published=1&featured_flag=1&ordering=-created_at&`}
+            heading="Featured Poems"
+          />
         </Col>
-        <Col md={4}>
+        {/* featured profiles for large screen sizes */}
+        <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
           <FeaturedProfiles />
         </Col>
       </Row>
